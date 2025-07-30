b296cb83f9aedd55a37f761e36d5fea91937c79a
mominulislamsharon <islamikchannel328250@gmail.com>
2025-07-30 06:07:51 +0600

    added clerk authentication singin-up navbar

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/navbar';
+import React from 'react';
+
+const DashboardPage = () => {
+  return (
+    <div className='min-h-screen bg-gray-50'>
+      <Navbar />
+      <h1>Dashbaordpage</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
